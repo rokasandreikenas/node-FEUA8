27732396871ff0406c5282e3594a929636f1a333
rokasandreikenas <shyptt@gmail.com>
2023-10-02 19:27:29 +0300

    Add node.js and express intro

---
+++ 1_paskaita_express/index.js
@@ -0,0 +1,36 @@
+const express = require("express"); // importuojam express modulį (nepamirštam įsirašyti)
+const app = express(); // sukuriam express aplikaciją
+const port = 3000;
+
+const users = ["Alex", "Rose", "Megan"];
+
+// app.get(kelias, callback funkcija) - aprašomas API
+// kelias - kuriuo URL užeisime
+// callback funkcija - funkcija kuri bus įvykdita kai užeis į mūsų kelią
+app.get("/", (req, res) => {
+  res.send("Server is working"); // išsiunčiami duomenys kvietėjui
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+// :firstLetter - dinaminis route (dvitaškis nurodo, kad tai yra dinaminis)
+app.get("/users/:firstLetter", (req, res) => {
+  const { firstLetter } = req.params;
+  // /users/R => ["Rose"]
+  // /users/M => ["Megan"]
+  // /users/K => []
+
+  const foundUsers = users.filter((user) => user[0] === firstLetter);
+  res.send(foundUsers);
+});
+
+app.get("/fruits", (req, res) => {
+  res.send(["Apple", "Banana"]);
+});
+
+// paleidžiamas serveris, kuris klausosi mūsų nurodytu portu
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # node-FEUA8
+
 Node introduction
+
+- Informational responses (100 – 199)
+- Successful responses (200 – 299)
+- Redirection messages (300 – 399)
+- Client error responses (400 – 499) (Kliento klaida)
+- Server error responses (500 – 599) (Serverio klaida)
+- 200 OK, 201 Created, 204 No Content
+- 400 Bad Request, 401 Unauthorized, 403 Forbidden, 404 Not Found
+- 500 Internal Server Error
+- req (request) - tai ką gauname į serverį
+- res (response) - tai ką išsiunčiam iš serverio
